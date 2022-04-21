5ab07387d3c85205b5aa61021ec7979640ac691b
Louis Claffey <85559012+LouisClaffey@users.noreply.github.com>
2022-04-21 18:51:30 +0100

    small update
    changed other methods to only run if user login details are correct

--- louis_claffey_distr_sys_smart_city_ca1/src/main/java/ReportProblemService/ReportProblemClient.java
+++ louis_claffey_distr_sys_smart_city_ca1/src/main/java/ReportProblemService/ReportProblemClient.java
@@ -137,63 +137,6 @@ public class ReportProblemClient {
 				return;
 			}
 			
-			
-//			System.out.println("The problem type is: " + number);
-//			System.out.println("The fixes are as follows:\n ");
-//			
-//			// iterate through the messages declared in server -- server side streaming
-//			Iterator<problemFixes> messages = bstub.reportProblem(number);
-//			
-//			while (messages.hasNext()) {
-//				
-//				problemFixes aResponse = messages.next();
-//				System.out.println(aResponse.getNumber());
-//			}
-//			
-//			// client side streaming
-//			
-//			
-//			StreamObserver<problemFix> responseObserver = new StreamObserver<problemFix>() {
-//	
-//				@Override
-//				public void onNext(problemFix value) {
-//					// we will get the fix from our server 
-//					System.out.println("Final response from the server.\n" + value.getFix());
-//					
-//				}
-//	
-//				@Override
-//				public void onError(Throwable t) {
-//					System.out.println("There was an error.");
-//					t.printStackTrace();
-//					
-//					
-//				}
-//	
-//				@Override
-//				public void onCompleted() {
-//					System.out.println("Completed.");
-//					
-//				}};
-//				
-//				
-//				// sending the messages from the client via the gRPC library and the new stream observer
-//				
-//				
-//				StreamObserver<problemType> requestObserver = asyncStub.reportWarning(responseObserver);
-//				
-//				requestObserver.onNext(problemType.newBuilder().setNumber(1).build());
-//				requestObserver.onNext(problemType.newBuilder().setNumber(2).build());
-//				requestObserver.onNext(problemType.newBuilder().setNumber(3).build());
-//			
-//				
-//				System.out.println("client has now sent its messages.\n");
-//				
-//				requestObserver.onCompleted();
-//				
-//				// finally before shutting down, we logout. 
-//				
-//				empty logOutResponse = bstub.logOut(logout);
 				
 		} catch (StatusRuntimeException e) {
 		    logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
